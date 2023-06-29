1fd129c78eb6e71b35adb146e72f8549fb94c3f6
yeop0740 <ssucgoo@gmail.com>
2023-06-29 20:32:29 +0900

    feat: 주문, 주문 취소 기능 구현

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,66 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송 정보 설정
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        /*
+        Order 엔티티의 orderItems, delivery 에 CASCADE 가 걸려있기 때문에 persist() 를 ORder 객체에만 해주어도
+        orderItem, delivery 객체에 각각 persist() 가 실행된다.
+        Order 객체에서만 orderItem, delivery 를 사용하기 때문에 CASCADE 를 적용할 수 있는 것임.(order 과 lifecycle 이 동일함)
+        만약 다른 도메인에서 orderItem 이나 delivery 를 사용하면 각각 repository 를 생성하여 따로 관리해 주어야 한다.
+         */
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        // 주문 취소
+        order.cancel();
+    }
+}
