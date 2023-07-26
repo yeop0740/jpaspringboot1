98042e8ab1b9de2a7219e1614a57e148ba4c07c8
yeop0740 <ssucgoo@gmail.com>
2023-07-27 00:25:06 +0900

    feat: 모든 회원 조회 api 구현(Entity)

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,8 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
@@ -55,6 +57,24 @@ public class MemberApiController {
         memberService.update(id, request.getName());
         Member findMember = memberService.findOne(id);
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+
+    }
+
+    /**
+     * 조회 V1: 응답 값으로 엔티티를 직접 외부에 노출한다.
+     * 문제점
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     * - 기본적으로 엔티티의 모든 값이 노출된다.
+     * - 응답 스펙을 맞추기 위해 로직이 추가된다. (@JasonIgnore, 별도의 뷰 등등)
+     * - 실무에서는 같은 엔티티에 대해 API 가 용도에 따라 다양하게 만들어지는데, 한 엔티티에 각각의 API 를 위한 프레젠테이션 응답 로직을 담기는 어렵다.(별도의 Result 클래스 생성으로 해결)
+     * 결론
+     * - API 응답 스펙에 맞추어 별도의 DTO 를 반환한다.
+     */
+    // 조회 V1: 안 좋은 버전, 모든 엔티티가 노출, @JasonIgnore -> 엔티티에 적용하기 좋지 않은 어노테이션, api 가 이거 하나가 아니기 때문이다. 화면에 종속적이면 안된다.
+    @GetMapping("/api/v1/members")
+    public List<Member> memberV1() {
+
+        return memberService.findMembers();
 
     }
 
